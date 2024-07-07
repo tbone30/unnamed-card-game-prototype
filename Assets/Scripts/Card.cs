5bccd578374fa18a9e05565d5e2cad5e316ca757
tbone30 <twelch8585@gmail.com>
2024-07-06 21:09:18 -0400

    reformat to build in unity

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tbone
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+
+    public class Card: ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardType;
+        public int health;
+        public int damageMin;
+        public int damageMax;
+        public List<DamageType> damageType;
+
+        public enum CardType
+        {
+            Fire,
+            Earth,
+            Water,
+            Dark,
+            Light,
+            Air
+        }
+
+        public enum DamageType
+        {
+            Fire,
+            Earth,
+            Water,
+            Dark,
+            Light,
+            Air
+        }
+    }
+}
