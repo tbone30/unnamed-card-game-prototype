58f556ae768bd024c87c5e28b2840f01929897d8
tbone30 <74034535+tbone30@users.noreply.github.com>
2024-07-09 13:06:23 -0400

    added hand manager, deck

---
+++ Assets/Scripts/DeckManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using tbone;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<Card> allCards = new List<Card>(); //List of all cards in the deck
+    private int currentIndex = 0; //Index of the current location in the deck
+
+    void Start()
+    {
+        //Load all card assets from the Resources folder
+        Card[] cards = Resources.LoadAll<Card>("Cards");
+        print(cards.Length);
+        //Add the loaded cards to the allCards list
+        allCards.AddRange(cards); 
+    }
+    
+    public void DrawCard(HandManager handManager)
+    {
+        if(allCards.Count == 0)
+        {
+            return;
+        }
+        
+        Card nextCard = allCards[currentIndex]; //Get the next card in the deck
+        handManager.AddCardToHand(nextCard); //Add the card to the hand
+        currentIndex = (currentIndex + 1) % allCards.Count; //Increment the index and loop back to the start if necessary
+    }
+}
---
+++ Assets/Scripts/HandManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using tbone;
+
+public class HandManager : MonoBehaviour
+{
+    public DeckManager deckManager; //Reference to the deck manager
+    public GameObject cardPrefab; //Assign card prefab in inspector
+    public Transform handTransform; //Root of the hand position
+    public float fanSpread = 7.5f; //How spread out the hand is
+    public float cardSpacing = -100f; //How much space is between cards
+    public float verticalSpacing = 100f; //How much space is between cards
+    public List<GameObject> cardsInHand = new List<GameObject>(); //Holds list of the card objects currently in hand
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void AddCardToHand(Card cardData)
+    {
+        //Instantiate a new card object
+        GameObject newCard = Instantiate(cardPrefab, handTransform.position, Quaternion.identity, handTransform);
+        cardsInHand.Add(newCard); //Add the new card to the list of cards in hand
+
+        //Set card data of instantiated card
+        newCard.GetComponent<CardDisplay>().cardData = cardData;
+
+        UpdateHandVisuals();
+    }
+
+    void Update()
+    {
+        UpdateHandVisuals();
+    }
+
+    private void UpdateHandVisuals()
+    {
+        int cardCount = cardsInHand.Count;
+
+        if(cardCount == 1)
+        {
+            cardsInHand[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            cardsInHand[0].transform.localPosition = new Vector3(0f, 0f, 0f);
+            return;
+        }
+
+        for(int i = 0; i<cardCount; i++)
+        {
+            //Rotate the card around the z-axis to fan them out
+            float rotationAngle = (fanSpread * (i - (cardCount - 1) / 2f));
+            cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+
+            float normalizedPosition = (2f * i / (cardCount - 1) - 1f); //Normalize card position between -1 and 1
+            float horizontalOffset = (cardSpacing * (i - (cardCount - 1) / 2f)); //Spread the cards horizontally
+            float verticalOffset = verticalSpacing * (1 - normalizedPosition * normalizedPosition); //Spread the cards vertically
+            cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticalOffset, 0f);
+        }
+    }
+}
